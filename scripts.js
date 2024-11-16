94732d3c899eed6209ff1dc43e4fd3e471ca6d6e
daniellypedrini <pedrinidanielly94@gmail.com>
2024-11-16 00:40:17 -0300

    Calculador

---
+++ scripts.js
@@ -0,0 +1,19 @@
+function adicionarCaracter(caracter){
+    const valorInput = document.querySelector('.display').value
+    document.querySelector('.display'). value = valorInput + caracter
+}
+
+function limparTela(){
+    document.querySelector('.display'). value = ''
+}
+
+function calcular(){
+    const valorInput = document.querySelector('.display').value
+    document.querySelector('.display'). value = eval(valorInput)
+}
+function invert(){
+    const valorInput = document.querySelector('.display').value
+    document.querySelector('.display'). value = valorInput * -1
+
+
+}
